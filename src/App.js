d677c9f97160aaa34593fcabfb42e796de2d9505
jules <congtuyen598@gmail.com>
2019-07-12 15:54:16 +0700

    react lifecycles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCounter: true
+    };
+  }
+
+  removeCounter() {
+    this.setState({
+      showCounter: false
+    });
+  }
+
+  componentDidUpdate() {
+    console.log('App updated');
+  }
+
+  render() {
+    console.log('app render');
+    return (
+      <div className="App">
+        <button onClick={() => this.removeCounter()}>Remove Counter</button>
+        { this.state.showCounter && <Counter /> }
+      </div>
+    );
+  }
 }
 
 export default App;
